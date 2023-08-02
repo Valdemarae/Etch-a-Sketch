eb8ab800f4f1c03ee906067c27080bf823be8b7b
Valdemar <valdemar.kobelis@gmail.com>
2023-08-02 13:45:27 +0300

    Create 16x16 grid

---
+++ javascript.js
@@ -0,0 +1,15 @@
+const container = document.querySelector('.container');
+
+function grid () {
+    for (let i = 0; i < 16; i++) {
+        const line = document.createElement('div');
+        line.classList.add('line');
+        for (let j = 0; j < 16; j++) {
+            const block = document.createElement('div');
+            block.classList.add('block');
+            line.appendChild(block);
+        }
+        container.appendChild(line);
+    }
+}
+grid();
